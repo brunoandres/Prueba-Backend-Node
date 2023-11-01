9e916dfbeb5fb600bada9f9aaf87af4ed53356b7
brunoandres <brunoandres2013@gmail.com>
2023-11-01 12:52:09 -0300

    .

--- db/typeorm.config.ts
+++ db/typeorm.config.ts
@@ -8,14 +8,19 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'mysql',
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "",
-  database: "new_developers",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 3306, 
+  username: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_DATABASE,
   entities: [`${__dirname}/../src/**/*.entity{.ts,.js}`],
-  synchronize: configService.get('nodenv') === 'development',
-  logging: configService.get('nodenv') === 'development',
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
   migrations: [`${__dirname}/migrations/*{.ts,.js}`],
   migrationsTableName: 'migrations',
 });
+
+
+
+
+
